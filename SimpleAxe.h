64bffeb594583b1dad89fbb1483050316ec80a22
Austin <medinajaustin@gmail.com>
2017-10-10 12:43:08 -0400

    Add SimpleAxe weapon
    Implement Simple Axe that does 60 hitpoints. Ignores armor if it
    is less than 20.

---
+++ SimpleAxe.h
@@ -0,0 +1,18 @@
+#include <string>
+#include "Weapon.h"
+
+#ifndef SIMPLEAXE_H
+#define SIMPLEAXE_H
+
+
+class SimpleAxe : public Weapon {
+public:
+
+    SimpleAxe() : Weapon("Simple axe", 60.0) {
+    }
+    virtual ~SimpleAxe() {}; 
+    virtual double hit(double armor);
+
+};
+
+#endif /* SIMPLEAXE_H */
